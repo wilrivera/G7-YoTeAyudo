c9266d7869e87ab9075f976e1c3bfff266335e94
wilrivera <wilmmer.rivera@gmail.com>
2022-03-27 21:16:21 -0500

    cambios por Wilmer

---
+++ app/src/main/java/com/yoteayudo/yoteayudo/RegistrarActivity.java
@@ -0,0 +1,17 @@
+package com.yoteayudo.yoteayudo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Registrar Cuenta");
+        setContentView(R.layout.activity_registrar);
+    }
+}

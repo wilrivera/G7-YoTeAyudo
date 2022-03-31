40d83c85aa1d062ba4b9caead4a616b05055dd0f
wilrivera <wilmmer.rivera@gmail.com>
2022-03-30 20:14:48 -0500

    Se agrego loyout de cupones, comparar y perfil
    Se agrego loyout de cupones, comparar y perfil

--- app/src/main/java/com/yoteayudo/yoteayudo/MainActivity.java
+++ app/src/main/java/com/yoteayudo/yoteayudo/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_perfil, R.id.nav_cupon, R.id.nav_favoritos, R.id.nav_compartir, R.id.nav_buscarcomparar)
+                R.id.nav_perfil, R.id.nav_cupon, R.id.nav_favoritos, R.id.nav_compartir, R.id.nav_buscarcomparar, R.id.nav_comparar)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
---
+++ app/src/main/java/com/yoteayudo/yoteayudo/ui/CompararFragment.java
@@ -0,0 +1,6 @@
+package com.yoteayudo.yoteayudo.ui;
+
+import android.app.Activity;
+
+public class CompararFragment extends Activity {
+}
